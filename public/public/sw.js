5260737ad7e4f50ac277747eec79f4b4247e070c
ovikeltrader <ovikeltrader@gmail.com>
2026-05-15 19:25:52 -0300

    Update sw.js

--- public/public/sw.js
+++ public/public/sw.js
@@ -1,1 +1,3 @@
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
